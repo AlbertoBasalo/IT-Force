a36119faba35ffc16836c73bb788d6ae92a1d7e7
Alberto Basalo <albertobasalo@hotmail.com>
2019-11-21 19:58:21 +0100

    angular template syntax

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
+  title = "Abanca - IT force";
+  dueDate = new Date();
+  isDisabled = true;
+  imageAlt = "abanca staff";
+  isGreen = false;
+  isCyan = false;
+  myNumber = 0;
+  customers = ["Amancio", "Bill", "Jeff", "Mark"];
+  provincias = [
+    { cod: 15, texto: "A Coruña" },
+    { cod: 27, texto: "Lugo" },
+    { cod: 34, texto: "Ourense" },
+    { cod: 36, texto: "Pontevedra" }
+  ];
+  provinciaSeleccionada;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  isOdd() {
+    return this.myNumber % 2 != 0;
   }
 
+  clickOnActivate() {
+    this.isDisabled = !this.isDisabled;
+    this.isCyan = true;
+  }
 }
