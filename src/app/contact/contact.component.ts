2270cc922500c3721002c339f2ac1ba4afca12e7
Alberto Basalo <albertobasalo@hotmail.com>
2019-11-25 17:42:09 +0100

    reactive form module

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-contact',
-  templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  selector: "app-contact",
+  templateUrl: "./contact.component.html",
+  styleUrls: ["./contact.component.css"]
 })
 export class ContactComponent implements OnInit {
+  contactForm: FormGroup;
+  private contact = {
+    contactName: "",
+    email: "",
+    subject: "",
+    age: 1
+  };
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(formBuilder: FormBuilder) {
+    this.contactForm = formBuilder.group({
+      contactName: [this.contact.contactName, Validators.required],
+      email: [this.contact.email, [Validators.required, Validators.email]],
+      subject: [
+        this.contact.subject,
+        [Validators.required, Validators.minLength(4)]
+      ],
+      age: [this.contact.age, [Validators.min(18), Validators.max(99)]]
+    });
   }
 
+  ngOnInit() {}
+
+  saveClick() {
+    console.log(this.contactForm.value);
+    console.log(this.contact);
+    this.contact = this.contactForm.value;
+    this.contact.email = this.contact.email.toUpperCase();
+    console.log(this.contact);
+  }
+
+  hasErrors(controlName: string) {
+    return this.contactForm.controls[controlName].invalid;
+  }
+
+  isTouched(controlName: string) {
+    return this.contactForm.controls[controlName].touched;
+  }
+
+  hasError(controlName: string, errorName: string) {
+    const control = this.contactForm.controls[controlName];
+    return control.hasError(errorName);
+  }
 }
--- src/app/contact/contact.module.ts
+++ src/app/contact/contact.module.ts
@@ -1,10 +1,17 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
 import { ContactRoutingModule } from "./contact-routing.module";
 import { ContactComponent } from "./contact.component";
 
 @NgModule({
   declarations: [ContactComponent],
-  imports: [CommonModule, ContactRoutingModule]
+  imports: [
+    MatButtonModule,
+    CommonModule,
+    ContactRoutingModule,
+    ReactiveFormsModule
+  ]
 })
 export class ContactModule {}
