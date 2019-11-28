0a21e40c61a795df5937469ef8442f40c0a491cd
Alberto Basalo <albertobasalo@hotmail.com>
2019-11-28 17:41:28 +0100

    interface

--- src/app/exchange/exchange.component.ts
+++ src/app/exchange/exchange.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Component, OnInit } from "@angular/core";
+import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
 
 @Component({
@@ -9,16 +10,22 @@ import { map } from "rxjs/operators";
 })
 export class ExchangeComponent implements OnInit {
   private url = "https://api.exchangeratesapi.io/latest?symbols=USD,GBP";
-  rates;
-  rates$;
+  rates: IRate;
+  rates$: Observable<object>;
   constructor(private http: HttpClient) {
-    this.http.get<any>(this.url).subscribe({
-      next: data => (this.rates = data.rates)
+    this.http.get<IRate>(this.url).subscribe({
+      next: data => (this.rates = data)
     });
     console.log(this.url);
 
-    this.rates$ = this.http.get<any>(this.url).pipe(map(data => data.rates));
+    this.rates$ = this.http.get<IRate>(this.url).pipe(map(data => data.rates));
   }
 
   ngOnInit() {}
+}
+
+interface IRate {
+  rates: object;
+  base: string;
+  date: Date;
 }
